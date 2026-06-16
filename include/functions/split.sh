883b7d0481070fbd3bd15ecc9e9d6c8b2c44b48c
pagedmov <kylerclay@proton.me>
2026-06-15 20:14:43 -0400

    feat: the split function can now take input via stdin
    e.g. `... | split : | ...`

--- include/functions/split.sh
+++ include/functions/split.sh
@@ -1,15 +1,38 @@
 split() {
-	[ "$#" -eq 2 ] || raise "Usage: split <%1> <%2> " 'pattern' 'string'
-	[ -z "$2" ] && return
-	local pat="$1"
-	local parts="$2"
-	local part
+	[ "$#" -ge 1 ] || raise "Usage: split <%1> [<%2>] " 'pattern' 'string'
+	local pat parts part
 
-	while parts="${parts%${pat}}"; do :; done # strips all trailing delimiters
+  while getopts ":0" opt; do
+    case "$opt" in
+      0) pat=$'\0' ;;
+      *) raise "Usage: split <%1> [<%2>] " 'pattern' 'string' ;;
+    esac
+  done
+  if [ -z "$pat" ]; then
+    pat="$1"
+  fi
 
-	parts="${parts}${pat}" # attaches a delimiter to the end
+  if ! [ -t 0 ]; then
+    while IFS= read -r parts; do
+      while parts="${parts%"${pat}"}"; do :; done # strips all trailing delimiters
 
-	while part="${parts%%${pat}*}" && parts="${parts#*${pat}}"; do
-		quote "$part";
-	done
+      parts="${parts}${pat}" # attaches a delimiter to the end
+
+      while part="${parts%%"${pat}"*}" && parts="${parts#*"${pat}"}"; do
+        quote "$part";
+      done
+    done
+  elif [ -z "$2" ]; then
+    return
+  else
+    parts="$2"
+    while parts="${parts%"${pat}"}"; do :; done # strips all trailing delimiters
+
+    parts="${parts}${pat}" # attaches a delimiter to the end
+
+    while part="${parts%%"${pat}"*}" && parts="${parts#*"${pat}"}"; do
+      quote "$part";
+    done
+  fi
+
 }
