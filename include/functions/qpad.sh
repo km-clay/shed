29c2a80fa1b41895f46d8370ea173015b900eebc
pagedmov <kylerclay@proton.me>
2026-06-18 01:15:40 -0400

    feat: qpad now takes -n flag

--- include/functions/qpad.sh
+++ include/functions/qpad.sh
@@ -1,21 +1,43 @@
 qpad() {
-	local records=()
-	local max=0
+	local has_names=0
+
+	while getopts ":n" opt; do
+		case "$opt" in
+			n) has_names=1 ;;
+		esac
+	done
+	shift $((OPTIND - 1))
+
+	local target=0
+
+	local -a records=()
+	local -a headers=()
+
+	if ((has_names)); then
+		read -q -a headers
+		target="${#headers[@]}"
+	fi
 
 	while IFS= read -r line; do
 		push records "$line"
-		local fields=()
+		local -a fields
 		unquote -a fields "$line"
-		[ "${#fields[@]}" -gt "$max" ] && max="${#fields[@]}"
+		(( ${#fields[@]} > target )) && target=${#fields[@]}
 	done
 
+	if ((has_names)); then
+		while (( ${#headers[@]} < target )); do
+			push headers ""
+		done
+		quote "${headers[@]}"
+	fi
+
 	for record in "${records[@]}"; do
-		local fields=()
+		local -a fields
 		unquote -a fields "$record"
-		while [ "${#fields[@]}" -lt "$max" ]; do
+		while (( ${#fields[@]} < target )); do
 			push fields ""
 		done
-
 		quote "${fields[@]}"
 	done
 }
