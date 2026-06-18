ad190ffad761daeda6cd9003400de0be47f177c7
pagedmov <kylerclay@proton.me>
2026-06-18 01:15:56 -0400

    fix: qwhere now correctly shifts arguments after getopts loop

--- include/functions/qwhere.sh
+++ include/functions/qwhere.sh
@@ -1,14 +1,32 @@
 qwhere() {
-	local body="$1"
 	local has_names=0
-	
+
 	while getopts ":n" opt; do
 		case "$opt" in
 			n) has_names=1 ;;
 		esac
 	done
-	
-	eval "__qwhere_lambda() { $body; }"
+	shift $((OPTIND - 1))
+
+	local body="$1"
+
+	if ((has_names)); then
+		local header_line
+		IFS= read -r header_line
+		local -a headers
+		unquote -a headers "$header_line"
+
+		local prelude=""
+		for i in "${!headers[@]}"; do
+			prelude+="local ${headers[i]}=\"\${$((i+1))}\"; "
+		done
+		eval "__qwhere_lambda() { $prelude $body; }"
+
+		printf '%s\n' "$header_line"
+	else
+		eval "__qwhere_lambda() { $body; }"
+	fi
+
 	defer unset -f __qwhere_lambda
 
 	while IFS= read -r line; do
