b6b3b9f1e33954e395912cd593d476da4f8d52dc
pagedmov <kylerclay@proton.me>
2026-06-19 16:01:50 -0400

    tweak: sqr_ls.sh now uses 'command ls' and splits fields using 'vice'

--- examples/sqr_ls.sh
+++ examples/sqr_ls.sh
@@ -1,28 +1,15 @@
 qls() {
-	# 'ls' output formatted in SQR
-	
-	emit_fields() {
-		quote "${fields[0]}" \
-			"${fields[1]}" \
-			"${fields[2]}" \
-			"${fields[*]:3:3}" \
-			"${fields[*]:6}"
-	}
-	defer unset -f emit_fields
+	local SQR_TABLE_JUSTIFY="left"
 
-	local buf=""
-	local first=0
-	ls -l | while read -q -a fields; do
-		if (( first == 0 )); then
-			buf+=$(emit_fields)
-			first=1
-			else
-			buf+=$'\n'
-			buf+=$(emit_fields)
-		fi
-	done
-
-	# __emit_sqr is a helper included in include/functions
-	# it pipes to qtable if stdout is a tty, and passes through otherwise
-	echo "$buf" | qname mode size user date name | __emit_sqr -n
+	command ls -l | tail -n +2 \
+		| vice --lines --quoted --sep 'W' \
+			-c 'viW' \
+			-m 'W' \
+			-r 2:1 \
+			-c 'viW' \
+			-c 'viWEE' \
+			-c '$' \
+		| qname mode user size date name \
+		| qselect -n name size user date mode \
+		| __emit_sqr -n
 }
