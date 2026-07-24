ae6651b15470d90048d52d476bdf30c44c27bb3c
pagedmov <kylerclay@proton.me>
2026-07-24 14:34:05 -0400

    fix: minute counting bug in 'times' builtin

--- src/builtin/times.rs
+++ src/builtin/times.rs
@@ -19,19 +19,19 @@ impl super::Builtin for Times {
     let child_sys = CmdTimer::tv_to_ms(child_usage.system_time());
 
     let self_user_minutes = self_user / 60_000;
-    let self_user_seconds = self_user / 1000;
+    let self_user_seconds = (self_user / 1000) % 60;
     let self_user_milliseconds = self_user % 1000;
 
     let self_sys_minutes = self_sys / 60_000;
-    let self_sys_seconds = self_sys / 1000;
+    let self_sys_seconds = (self_sys / 1000) % 60;
     let self_sys_milliseconds = self_sys % 1000;
 
     let child_user_minutes = child_user / 60_000;
-    let child_user_seconds = child_user / 1000;
+    let child_user_seconds = (child_user / 1000) % 60;
     let child_user_milliseconds = child_user % 1000;
 
     let child_sys_minutes = child_sys / 60_000;
-    let child_sys_seconds = child_sys / 1000;
+    let child_sys_seconds = (child_sys / 1000) % 60;
     let child_sys_milliseconds = child_sys % 1000;
 
     outln!(
