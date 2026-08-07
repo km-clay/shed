30bcc9a97481b9a4e079c41c36ada32651d420a6
pagedmov <kylerclay@proton.me>
2026-08-07 01:13:50 -0400

    bump to v0.38.6

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
 
     packages.default = rustPlatform.buildRustPackage {
       pname = "shed";
-      version = "0.38.5";
+      version = "0.38.6";
 
       src = self;
       cargoLock = { lockFile = ./Cargo.lock; };
