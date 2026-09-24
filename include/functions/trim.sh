479075794c99be06b27f8f0fce58f748505d0dc3
pagedmov <kylerclay@proton.me>
2026-09-24 02:32:02 -0400

    refactor: improved `trim` function logic, added stdin path

--- include/functions/trim.sh
+++ include/functions/trim.sh
@@ -1,8 +1,22 @@
 trim() {
-	local parts=()
-	while [ "$#" -gt 0 ]; do
-		parts+=$(quote $1)
-		shift
-	done
-	quote "${parts[@]}"
+	local parts=() s
+
+  if [ "$#" -gt 0 ]; then
+    while [ "$#" -gt 0 ]; do
+      s="$1"
+      s="${s#"${s%%[![:space:]]*}"}"
+      s="${s%"${s##*[![:space:]]}"}"
+      push parts "$s"
+      shift
+    done
+    quote "${parts[@]}"
+  elif ! [ -t 0 ]; then
+    while IFS= read -r line || [ -n "$line" ]; do
+      line="${line#"${line%%[![:space:]]*}"}"
+      line="${line%"${line##*[![:space:]]}"}"
+      quote "$line"
+    done
+  else
+    return 1
+  fi
 }
